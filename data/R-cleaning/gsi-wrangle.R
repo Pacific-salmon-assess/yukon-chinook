5674c9934f550c5f0e90edc312934b5216eff489
DylanMG <dylan.glaser1@ucalgary.ca>
2023-02-14 17:45:19 -0800

    fix gsi w/ brendan's suggestions

--- data/R-cleaning/gsi-wrangle.R
+++ data/R-cleaning/gsi-wrangle.R
@@ -8,6 +8,10 @@ library(readxl)
 old_gsi <- read.csv(here("data/gsi/gsiSamplesAllProbs.csv")) |>
   rename(fish = sample_num)
 
+regions <- old_gsi |>
+  select(region, region_name)|>
+  distinct()
+
 new_gsi <- read_xlsx(here("data/gsi/PID20200058-20210137_EagleSonar_(20-21)_sc270-371_2022-12-20.xlsx"),
                      sheet = "custom_table_ids") |>
   select(-collection, -mixture_collection, -ID_Source, -PBT_brood_year) 
@@ -35,9 +39,12 @@ clean_new_gsi <- rbind(gsi1, gsi2, gsi3, gsi4, gsi5) |>
   filter(!is.na(prob)) |>
   separate(indiv, into = c(NA, "year", "julian", "fish"), sep="_") |>
   mutate_at(c("year", "julian", "fish"), as.integer) |>
-  rename(region_name=group)
+  mutate(data_label = "EagleSonar", 
+         gear = "Test Fishery") |>
+  rename(region_name = group) |>
+  left_join(regions, by = "region_name")
 
-all_gsi <- bind_rows(old_gsi, new_gsi)
+all_gsi <- bind_rows(old_gsi, clean_new_gsi)
 
 #write it---------------------------------------------------------------------------------
 write.csv(all_gsi, here("data/cleaned-data/gsi.csv"), row.names = FALSE)
