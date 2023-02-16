dbcc64e27140d2bb28e944d7cb39dc42fea39875
Connors <Brendan.Connors@dfo-mpo.gc.ca>
2023-02-15 16:55:38 -0800

    added script to derive mean length comps at border

--- data/R-cleaning/asl-wrangle.R
+++ data/R-cleaning/asl-wrangle.R
@@ -1,4 +1,4 @@
-#wrangling ASL data into 2 matrices
+#wrangling ASL data into 3 matrices
 
 library(tidyverse)
 library(here)
@@ -13,7 +13,7 @@ my_replace <- function(x){
 }
 
 #read in and wrangle tables-------------------------------------------------------------- 
-asl <- read.csv(here("data/asl/yukon-chinook-ASL-ADFG-database-acessed-13Feb2023.csv")) |>
+as <- read.csv(here("data/asl/yukon-chinook-ASL-ADFG-database-acessed-13Feb2023.csv")) |>
   select(Sample.Year, Gear, Sex, Fresh.Water.Age, Salt.Water.Age) |>
   filter(!is.na(Fresh.Water.Age) & !is.na(Salt.Water.Age)) |>
   mutate(Age = paste0(Fresh.Water.Age, ".", Salt.Water.Age)) |>
@@ -27,20 +27,38 @@ asl <- read.csv(here("data/asl/yukon-chinook-ASL-ADFG-database-acessed-13Feb2023
   arrange(Year, Sex) |>
   as.data.frame()
 
+asl <- read.csv(here("data/asl/yukon-chinook-ASL-ADFG-database-acessed-13Feb2023.csv")) |>
+  select(Sample.Year, Gear, Sex, Fresh.Water.Age, Salt.Water.Age, Length, Length.Measurement.Type) |>
+  filter(!is.na(Fresh.Water.Age) & !is.na(Salt.Water.Age)) |>
+  filter(Sex != "unknown") |>
+  mutate(Age = paste0(Fresh.Water.Age, ".", Salt.Water.Age)) |>
+  rename(Year = Sample.Year) |>
+  group_by(Year, Sex, Gear, Age, Length.Measurement.Type) |>
+  summarise(Mean.Length = mean(Length)) |>
+  arrange(as.numeric(Age)) |>
+  pivot_wider(names_from = Age, 
+              values_from = Mean.Length) |>
+  my_replace()|>
+  arrange(Year, Sex) |>
+  as.data.frame()
+
 har_age <- read.csv(here("data/harvest/YkCk_Harvest_byDistrTypeStockAge.csv"), 
                     skip = 5) |>
   filter(Stock == "Upper") |>
   mutate(River.Section = case_when(District <= 2 ~ "below_pilot",
                                    District >=3 & District<6 ~ "above_pilot", 
                                   District == 7 ~ "Canada")) |> 
+  mutate(Fishery.type = case_when(Fishery == "US_Subsistence" ~ "Subsistence",
+                                  Fishery == "US_Comm" ~ "Commercial")) |> 
   arrange(Year, Fishery, River.Section)
 
 colnames(har_age) <- gsub("Age", "", colnames(har_age))
 
 har_age <- har_age |>
-  select(Year, Fishery, River.Section, '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '2.2', 
+  select(Year, Fishery.type, River.Section, '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '2.2', 
          '2.3', '2.4', '2.5')
 
 #write tables-----------------------------------------------------------------------------
-write.csv(asl, here("data/cleaned-data/border-age-table.csv"), row.names = FALSE)
+write.csv(as, here("data/cleaned-data/border-age-table.csv"), row.names = FALSE)
+write.csv(asl, here("data/cleaned-data/border-length-table.csv"), row.names = FALSE)
 write.csv(har_age, here("data/cleaned-data/harvest-age-table.csv"), row.names = FALSE)
